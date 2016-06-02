f790ed800d7dd0ba8b1e33ee485049d67f211ce3
Zach Allen <zallen@pension1.local>
2016-06-02 08:40:14 -0500

    Documents

--- models/document.js
+++ models/document.js
@@ -2,32 +2,33 @@ var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 var documentSchema = new mongoose.Schema({
-	name: String,
-	description: String,
-	ownerId: String,
-	planId: String,
-	documentType: Number, // Uses "DocumentType" String Map
-	tasks: [{
-		taskId: String
-	}],
-	approvals: [{
-		approvedBy: String,
-		approvedOn: Date
-	}],
-	createdBy: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        unique: false,
-        dropDups: false,
-    },
-    createdOn: Date,
-	modifiedBy: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        unique: false,
-        dropDups: false,
-    },
-    modifiedOn: Date,
+  name: String,
+  description: String,
+  ownerId: String,
+  planId: String,
+  documentType: String,
+  path: String,
+  tasks: [{
+    taskId: String
+  }],
+  approvals: [{
+    approvedBy: String,
+    approvedOn: Date
+  }],
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    unique: false,
+    dropDups: false,
+  },
+  createdOn: Date,
+  modifiedBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    unique: false,
+    dropDups: false,
+  },
+  modifiedOn: Date,
 });
 
 module.exports = restful.model('Document', documentSchema);
--- router/routes/document.js
+++ router/routes/document.js
@@ -1,9 +1,114 @@
-var express = require('express');
-var router = express.Router();
+var Model = require('../../models/document');
+var RestFilter = require('../../components/RestFilter');
+var UserSecurity = require('../security');
 
-var Document = require('../../models/document');
+var readFilterSchema = {
+  "title": "Plan Schema",
+  "type": "object",
+  "properties": {
+		"_id": { "type":"string" },
+		"name": { "type":"string" },
+		"description": { "type":"string" },
+		"ownerId": { "type":"string" },
+		"planId": { "type":"string" },
+    "documentType": { "type":"string" },
+    "path": { "type":"string" },
+    "tasks": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+    		  "taskId": { "type":"string" },
+        },
+      },
+    }
+    "approvals": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+    		  "approvedBy": { "type":"string" },
+      		"approvedOn": { "type":"string" },
+        },
+      },
+    },
+		"createdBy": { "type":"string" },
+		"createdOn": { "type":"date" },
+		"modifiedBy": { "type":"string" },
+		"modifiedOn": { "type":"date" },
+	},
+}
 
-Document.methods(['get', 'put', 'post', 'delete']);
-Document.register(router, '/document');
+var writeFilterSchema = {
+  "title": "Plan Schema",
+  "type": "object",
+  "properties": {
+    "name": { "type":"string" },
+    "description": { "type":"string" },
+    "ownerId": { "type":"string" },
+    "planId": { "type":"string" },
+    "documentType": { "type":"string" },
+    "path": { "type":"string" },
+    "tasks": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "taskId": { "type":"string" },
+        },
+      },
+    }
+    "approvals": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "approvedBy": { "type":"string" },
+          "approvedOn": { "type":"string" },
+        },
+      },
+    },
+	},
+}
 
-module.exports = router;
+function findOne (user, id, callback) {
+	Model
+		.findOne({"_id": id})
+		.where({
+      $or: [{
+        "planId": { $in : user.plans }
+      }]
+    })
+		.exec(function (err, result) {
+			return callback(result);
+		});
+}
+
+function findMany (user, callback) {
+	Model
+		.find()
+		.sort([['name', 'ascending']])
+		.where({
+      $or: [{
+        "planId": { $in : user.plans }
+      }]
+    })
+		.exec(function (err, result) {
+			return callback(result);
+		});
+}
+
+module.exports = new RestFilter({
+	path : "/document",
+	model: Model,
+	readFilterSchema: readFilterSchema,
+	writeFilterSchema: writeFilterSchema,
+	findOne: findOne,
+	findMany: findMany,
+	securityRoles: {
+		create: UserSecurity.isActiveUser,
+		read: UserSecurity.isActiveUser,
+		update: UserSecurity.isActiveUser,
+		destroy: UserSecurity.isActiveUser,
+	}
+});
