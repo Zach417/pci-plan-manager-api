2006ff44fdd49d387a024ef42936457e60900572
Zach Allen <zallen@pension1.local>
2016-06-17 09:06:50 -0500

    Meeting entity

--- models/meeting.js
+++ models/meeting.js
@@ -2,27 +2,40 @@ var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 var meetingSchema = new mongoose.Schema({
-	name: String,
-	description: String,
-	date: Date,
-	meetingType: Number, // Uses "MeetingType" String Map
-	members: [{
-		userId: String,
-	}],
-	createdBy: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        unique: false,
-        dropDups: false,
-    },
-    createdOn: Date,
-	modifiedBy: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        unique: false,
-        dropDups: false,
-    },
-    modifiedOn: Date,
+  name: String,
+  description: String,
+  startTime: Date,
+  endTime: Date,
+	taskId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Task',
+    unique: false,
+    dropDups: false,
+  },
+	planId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Plan',
+    unique: false,
+    dropDups: false,
+  },
+  meetingType: String,
+  people: [{
+    name: String,
+  }],
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    unique: false,
+    dropDups: false,
+  },
+  createdOn: Date,
+  modifiedBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    unique: false,
+    dropDups: false,
+  },
+  modifiedOn: Date,
 });
 
 module.exports = restful.model('Meeting', meetingSchema);
--- router/routes/meeting.js
+++ router/routes/meeting.js
@@ -1,9 +1,98 @@
-var express = require('express');
-var router = express.Router();
+var Model = require('../../models/meeting');
+var RestFilter = require('../../components/RestFilter');
+var UserSecurity = require('../security');
 
-var Meeting = require('../../models/meeting');
+var readFilterSchema = {
+  "title": "Plan Schema",
+  "type": "object",
+  "properties": {
+		"_id": { "type":"string" },
+		"name": { "type":"string" },
+		"description": { "type":"string" },
+		"startTime": { "type":"date" },
+		"endTime": { "type":"date" },
+		"meetingType": { "type":"string" },
+		"taskId": { "type":"string" },
+		"planId": { "type":"string" },
+    "people": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+    		  "name": { "type":"string" },
+        },
+      },
+    },
+		"createdBy": { "type":"string" },
+		"createdOn": { "type":"date" },
+		"modifiedBy": { "type":"string" },
+		"modifiedOn": { "type":"date" },
+	},
+}
 
-Meeting.methods(['get', 'put', 'post', 'delete']);
-Meeting.register(router, '/meeting');
+var writeFilterSchema = {
+  "title": "Plan Schema",
+  "type": "object",
+  "properties": {
+    "name": { "type":"string" },
+		"description": { "type":"string" },
+		"startTime": { "type":"date" },
+		"endTime": { "type":"date" },
+		"meetingType": { "type":"string" },
+		"taskId": { "type":"string" },
+		"planId": { "type":"string" },
+    "people": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+    		  "name": { "type":"string" },
+        },
+      },
+    },
+	},
+}
 
-module.exports = router;
+function findOne (user, id, callback) {
+	Model
+		.findOne({"_id": id})
+		.where({
+      $or: [
+        {"planId": { $in : user.plans }},
+				{"createdBy": user._id}
+      ]
+    })
+		.exec(function (err, result) {
+			return callback(result);
+		});
+}
+
+function findMany (user, callback) {
+	Model
+		.find()
+		.sort([['name', 'ascending']])
+		.where({
+      $or: [{
+        {"planId": { $in : user.plans }},
+				{"createdBy": user._id}
+      }]
+    })
+		.exec(function (err, result) {
+			return callback(result);
+		});
+}
+
+module.exports = new RestFilter({
+	path : "/document",
+	model: Model,
+	readFilterSchema: readFilterSchema,
+	writeFilterSchema: writeFilterSchema,
+	findOne: findOne,
+	findMany: findMany,
+	securityRoles: {
+		create: UserSecurity.isActiveUser,
+		read: UserSecurity.isActiveUser,
+		update: UserSecurity.isActiveUser,
+		destroy: UserSecurity.isActiveUser,
+	}
+});
