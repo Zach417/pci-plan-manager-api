32b580f20604740c88f9b9bfd52662c90ad40c15
Zach Allen <zallen@pension-consultants.com>
2017-01-09 17:04:54 -0600

    added seperate file for mongoose connection settings

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://172.31.16.142/planmanager");
+var conf = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+var mongooseOptions = { server: { socketOptions: { keepAlive: 1 } } };
+var usr = conf.mongodb.username;
+var pwd = conf.mongodb.password;
+var ip = conf.mongodb.ip;
+var dbs = conf.mongodb.database;
+var connectionString = "mongodb://"+usr+":"+pwd+"@"+ip+"/"+dbs;
+mongoose.connect(connectionString, mongooseOptions);
 
 app.use(function (req, res, next) {
     console.log(req.method + req.path);
